df7a35e1bb57762ffbebe24868ccff84d75729ef
MAnjaparidze <anjaparidzemamuka@gmail.com>
2021-09-21 22:31:05 +0400

    Added Loading Animation & Redirectiong

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    handleRedirect();
+  }, [])
+
+  const handleRedirect = () => {
+    setTimeout(() => {
+      window.location.replace("https://www.youtube.com/c/FennecTechStudio");
+    }, 2000)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='loader'></div>
+      <div className='fennec_logo'></div>
     </div>
   );
 }
